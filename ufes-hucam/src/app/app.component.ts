9147d88604e5c2093df3fe5d9ad68dbaed4c7353
joaoa-casagrande <71766992+joaoa-casagrande@users.noreply.github.com>
2025-06-05 23:05:59 -0300

    inicial

---
+++ ufes-hucam/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ufes-hucam';
+
+  show = false;
+
+  toggleModal(v:boolean){
+    this.show = v;
+  }
+}
