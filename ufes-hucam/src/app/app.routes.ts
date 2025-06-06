f1601911d09a778126bf4e46d0582f7d777147d8
joaoa-casagrande <71766992+joaoa-casagrande@users.noreply.github.com>
2025-06-05 23:27:30 -0300

    residuos

--- ufes-hucam/src/app/app.routes.ts
+++ ufes-hucam/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import {Routes} from '@angular/router';
 import {SobreComponent} from "./pages/sobre/sobre.component";
 
 import {InicioComponent} from "./pages/inicio/inicio.component";
+import {GameComponent} from "./pages/game/game.component";
+import {ResiduosComponent} from "./pages/residuos/residuos.component";
 
 export const routes: Routes = [
 
@@ -10,6 +12,16 @@ export const routes: Routes = [
         path: 'sobre',
         component: SobreComponent,
 
+    },
+
+    {
+        path:'game',
+        component: GameComponent
+    },
+
+    {
+        path:'residuos',
+        component: ResiduosComponent
     },
 
     {
---
+++ ufes-hucam/src/app/pages/residuos/residuos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-residuos',
+  standalone: true,
+    imports: [
+        RouterLink
+    ],
+  templateUrl: './residuos.component.html',
+  styleUrl: './residuos.component.css'
+})
+export class ResiduosComponent {
+
+}
